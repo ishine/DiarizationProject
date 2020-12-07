29a861c203adb95aa65a07fddcf9e6bc89f45701
suchitra0911 <46502627+suchitra0911@users.noreply.github.com>
2020-12-07 12:16:06 -0800

    Update recall.py

--- recall.py
+++ recall.py
@@ -15,7 +15,7 @@ import matplotlib.pyplot as plt
 
 dict_map = {}
 
-dict_map_path = '/home/coder/suchitra/adosMod3/adosMod3_rttm/system/'
+dict_map_path = '/X/adosMod3/adosMod3_rttm/system/'
 
 for filename in glob.glob(os.path.join(dict_map_path,"*.txt")):
         with open(filename,'r') as f:
@@ -24,8 +24,8 @@ for filename in glob.glob(os.path.join(dict_map_path,"*.txt")):
    	                if 'mapped' in line:
                                 dict_map[line[3]+"_"+line[0]] = line[2]
 
-reference_path = '/home/coder/suchitra/adosMod3/adosMod3_rttm/reference/'
-system_path = '/home/coder/suchitra/adosMod3/adosMod3_rttm/system/'
+reference_path = '/X/adosMod3/adosMod3_rttm/reference/'
+system_path = '/X/adosMod3/adosMod3_rttm/system/'
 
 dict_reftime = {}
 dict_systime = {}
