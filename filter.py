1d556b05c3d64b3cb37db4fc0f27b1157e94a95a
suchitra0911 <46502627+suchitra0911@users.noreply.github.com>
2020-12-07 08:45:32 -0800

    Update filter.py

--- filter.py
+++ filter.py
@@ -15,9 +15,9 @@ dict3 = {}
 
 #function called to split into 3 sub directories depending on the age limits chosen
 def splitAge_function(list_path, name1, name2, name3):
-	pathname1 = os.path.join('/home/coder/suchitra/adosMod3/',name1)
-	pathname2 = os.path.join('/home/coder/suchitra/adosMod3/',name2)
-	pathname3 = os.path.join('/home/coder/suchitra/adosMod3/',name3)
+	pathname1 = os.path.join('/X/adosMod3/',name1)
+	pathname2 = os.path.join('/X/adosMod3/',name2)
+	pathname3 = os.path.join('/X/adosMod3/',name3)
 	if not os.path.exists(pathname1):	
 		os.mkdir(pathname1)
 	if not os.path.exists(pathname2):
@@ -36,8 +36,8 @@ def splitAge_function(list_path, name1, name2, name3):
 
 #function to split according to gender
 def splitGender_function(list_path, name1, name2):
-	pathname1 = os.path.join('/home/coder/suchitra/adosMod3/',name1)
-        pathname2 = os.path.join('/home/coder/suchitra/adosMod3/',name2)
+	pathname1 = os.path.join('/X/adosMod3/',name1)
+        pathname2 = os.path.join('/X/adosMod3/',name2)
 	if not os.path.exists(pathname1):
 		os.mkdir(pathname1)
 	if not os.path.exists(pathname2):
@@ -51,7 +51,7 @@ def splitGender_function(list_path, name1, name2):
 	
 
 #created 3 dictionaries depending on the pre-defined age limit.The values have been hardcoded depending on the min and max ages from the ageGenderIQ.sh file to give 3 almost equal splits. In this case, ages 3.5 to 7.6 years, ages 7.6 to 10 years and ages 10 and above.
-with open('/home/coder/suchitra/adosMod3/ageGenderIQ.sh','r') as f:
+with open('/X/adosMod3/ageGenderIQ.sh','r') as f:
 	for word in f:
                 word = word.split( )
 		list1.append(word[0])
@@ -67,8 +67,8 @@ with open('/home/coder/suchitra/adosMod3/ageGenderIQ.sh','r') as f:
 			list_female.append(word[0])
 
 
-wavfile_path = glob.glob(os.path.join('/home/coder/suchitra/adosMod3/adosMod3_wav/','*.wav'))
-rttmfile_path = glob.glob(os.path.join('/home/coder/suchitra/adosMod3/adosMod3_rttm','*.rttm'))
+wavfile_path = glob.glob(os.path.join('/X/adosMod3/adosMod3_wav/','*.wav'))
+rttmfile_path = glob.glob(os.path.join('/X/adosMod3/adosMod3_rttm','*.rttm'))
 
 splitAge_function(wavfile_path, "adosMod3_wav1", "adosMod3_wav2", "adosMod3_wav3")
 splitAge_function(rttmfile_path, "adosMod3_rttm1", "adosMod3_rttm2", "adosMod3_rttm3")
