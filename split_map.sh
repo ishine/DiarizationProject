c3d8a7f2fd6f5a7a74c2f74936bcb99a88a4b74c
suchitra0911 <46502627+suchitra0911@users.noreply.github.com>
2020-12-07 12:01:33 -0800

    Update split_map.sh

--- split_map.sh
+++ split_map.sh
@@ -11,8 +11,8 @@ mapfile -t my_array < new
 #echo "${my_array[@]}"
 
 for x in "${my_array[@]}"; do
-awk -v name=$x '{if ($2 == name) print $0}' rttm_modelnewb4PLDA > $x.rttm
-perl /X/CARE/dscore/scorelib/md-eval-22.pl -1 -c 0.25 -M `pwd`/map_$x.txt -r /home/coder/suchitra/CARE/rttm/reference/$x.rttm -s /home/coder/suchitra/CARE/rttm/system/$x.rttm
+awk -v name=$x '{if ($2 == name) print $0}' rttm_Model5PLDA > $x.rttm
+perl /X/CARE/dscore/scorelib/md-eval-22.pl -1 -c 0.25 -M `pwd`/map_$x.txt -r /X/CARE/rttm/reference/$x.rttm -s /X/CARE/rttm/system/$x.rttm
 done
 
 python /X/CARE/recall.py > /X/CARE/test5
